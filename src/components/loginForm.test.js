92d54407009317d5d9d2ca00a0e72be15d9112ff
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-11-07 17:44:40 +0100

    add task 02

---
+++ src/components/loginForm.test.js
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CatchError from "../components/CatchError";
+import LoginForm from "./LoginForm";
+
+/* 
+jest.mock("./LoginForm");
+
+describe("get()", () => {
+  it("should fetch ip when send request", async () => {
+    get.mockResolvedValueOnce({ ip: "100.100.100.100" });
+    const data = await get();
+    expect(data.ip).toBe("100.100.100.100");
+  });
+});
+ */
+
+describe("<LoginForm>", () => {
+  test("value of input is too short", async () => {
+    render(<LoginForm />);
+
+    const loginInput = await screen.findByRole("textbox", { name: /login/i });
+    userEvent.type(loginInput, "gg");
+    const error = await screen.findByText("The field is too short!");
+    expect(error).toBeInTheDocument();
+  });
+  test("value of input is valid", async () => {
+    render(<LoginForm />);
+
+    const loginInput = await screen.findByRole("textbox", { name: /login/i });
+    userEvent.type(loginInput, "ggdddd");
+    const error = screen.queryByText("The field is too short!");
+    expect(error).toBeNull();
+  });
+  test("submit of form - correct data", async () => {
+    const mock = jest.fn();
+    mock.mockReturnValueOnce(false);
+    render(
+      <CatchError>
+        <LoginForm tryAuth={mock} />
+      </CatchError>
+    );
+    const button = await screen.findByRole("button");
+    userEvent.click(button);
+    const error = await screen.findByText("Something went wrong");
+    expect(error).toBeInTheDocument();
+  });
+});
